5b285a10e199eabbf8348466f090700ef5f259be
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-10-27 14:57:41 +0100

    feat: added user avatar in Header

---
+++ front/src/layout/Header.tsx
@@ -0,0 +1,47 @@
+import { NavLink } from 'react-router-dom';
+
+const NAVBAR_ELEMENTS = {
+  HOME: {
+    path: '/',
+    label: 'Home',
+  },
+  DASHBOARD: {
+    path: '/dashboard',
+    label: 'Dashboard',
+  },
+};
+
+function Header() {
+  return (
+    <header className='flex bg-blue-500 p-5 text-white font-semibold items-center justify-between gap-x-10'>
+      <h1 className='text-2xl'>Evaluation meubles</h1>
+
+      <div className='flex items-center gap-x-10'>
+        <nav className='border-r-2 pr-10'>
+          <ul className='flex gap-x-8 justify-end text-xl '>
+            {Object.values(NAVBAR_ELEMENTS).map(({ path, label }) => (
+              <li key={path}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? 'opacity-100' : 'opacity-70')}
+                >
+                  {label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className='flex items-center gap-x-3'>
+          <span>Lucas Amsellem</span>
+          <img
+            src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimgcdn.stablediffusionweb.com%2F2024%2F12%2F11%2F35a41202-c7ab-467b-ac37-81e8f13d6eb7.jpg&f=1&nofb=1&ipt=eeb5fa124fe009ba9b7d79940295b800a7ada18ed996c293d0461a8a3e8b0a8d'
+            className='w-10 rounded-full'
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -4,31 +4,33 @@ import Furniture from '../components/Furniture';
 
 function HomePage() {
   return (
-    <ul className='flex flex-col gap-5'>
-      <Furniture
-        id={crypto.randomUUID()}
-        img={undefined}
-        name='Super étagère'
-        material='aluminium'
-        category='armoire'
-      />
+    <div>
+      <ul className='flex flex-col gap-5'>
+        <Furniture
+          id={crypto.randomUUID()}
+          img={undefined}
+          name='Super étagère'
+          material='aluminium'
+          category='armoire'
+        />
 
-      <Furniture
-        id={crypto.randomUUID()}
-        img={undefined}
-        name='Super armoire'
-        material='noyer'
-        category='étagère'
-      />
+        <Furniture
+          id={crypto.randomUUID()}
+          img={undefined}
+          name='Super armoire'
+          material='noyer'
+          category='étagère'
+        />
 
-      <Furniture
-        id={crypto.randomUUID()}
-        img={undefined}
-        name='Armoire en aluminium'
-        material='aluminium'
-        category='armoire'
-      />
-    </ul>
+        <Furniture
+          id={crypto.randomUUID()}
+          img={undefined}
+          name='Armoire en aluminium'
+          material='aluminium'
+          category='armoire'
+        />
+      </ul>
+    </div>
   );
 }
 
