9f22b3f514956b1fb45d74e5e3d28ddf5691d4b6
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-10-28 14:17:51 +0100

    feat: init backend

--- back/seed.js
+++ back/seed.js
@@ -1,27 +1,29 @@
-import Furniture from "./db/models/Furniture.js";
-import User from "./db/models/User.js";
-import data from "./data.js";
-import crypto from "crypto";
-import dotenv from "dotenv";
+import Furniture from './db/models/Furniture.js';
+import User from './db/models/User.js';
+import data from './data.js';
+import crypto from 'crypto';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 async function insertFurnitures() {
-	const { furnitures } = data;
+  const { furnitures } = data;
 
-	await Furniture.deleteMany({});
-	await Furniture.insertMany(furnitures);
-	console.log("Initial furnitures inserted");
-	process.exit(0);
+  await Furniture.deleteMany({});
+  await Furniture.insertMany(furnitures);
+  console.log('Initial furnitures inserted');
+  process.exit(0);
 }
 
-async function insertUser() {
-	const { user } = data;
+insertFurnitures();
 
-	await User.deleteMany({});
-	await User.insertOne(user);
-	console.log("Initial user inserted");
-	process.exit(0);
+async function insertUser() {
+  const { user } = data;
+
+  await User.deleteMany({});
+  await User.insertOne(user);
+  console.log('Initial user inserted');
+  process.exit(0);
 }
 
 insertUser();
--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,12 +1,23 @@
 import ActionButton from '../components/ActionButton';
-import Furniture from '../components/Furniture';
+// import Furniture from '../components/Furniture';
 import Modal from '../components/Modal';
 import useModal from '../hooks/useModal';
 import NewFurnitureForm from '../layout/NewFurnitureForm';
-import data from '../../../back/data.ts';
+import { useEffect } from 'react';
 
 function HomePage() {
   const { isOpen, toggleModal } = useModal();
+  // const [furnitures, setFurnitures] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setFurnitures(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className='flex flex-col'>
@@ -18,7 +29,7 @@ function HomePage() {
         <NewFurnitureForm />
       </Modal>
 
-      <ul className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+      {/* <ul className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
         {Object.values(data.furnitures).map(
           ({ description, title, image, quantity, category_id, materials }) => (
             <Furniture
@@ -32,7 +43,7 @@ function HomePage() {
             />
           )
         )}
-      </ul>
+      </ul> */}
     </div>
   );
 }
